c13e7261fec11d8678e0f93fc262fa1ee215f14b
Stephen Wiggins <steve.wiggins7@gmail.com>
2017-11-26 00:24:49 -0500

    configure dotenv, create json file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var tmi = require('tmi.js');
+var env = require('dotenv').config();
 
 var options = {
     options: {
@@ -9,8 +10,8 @@ var options = {
         reconnect: true
     },
     identity: {
-        username: "nuttybot_ow",
-        password: ""
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD
     },
     channels: ["avendor7"]
 };
@@ -25,5 +26,22 @@ client.on('connected', function(address,port){
 });
 
 client.on('chat', function (channel, user, message, self) {
-    client.action("avendor7", user['display-name'] + " is boosted");
-})
+    if (self) return;
+
+    fs.readFile('./people.json', 'utf8', function (err,data) {
+        data = JSON.parse(data); // you missed that...
+        for(var i = 0; i < data.length; i++) {
+            var newPerson = new Person();
+            newPerson.firstname = data[i].firstname;
+            newPerson.lastname = data[i].lastname;
+            newPerson.age = data[i].age;
+            newPerson.save(function (err) {});
+        }
+    });
+
+
+    if (message.startsWith("!")) {
+        client.action("avendor7", user['display-name'] + " is boosted");
+    }
+
+});
